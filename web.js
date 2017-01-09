c657436eac68ced74f59460bdc9aaeae21fa0cab
Gerhard <gerhard@ggoerlich.at>
2017-01-09 23:02:43 +0100

    fixed web.js

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
+var express = require('express');
+var logger = require('morgan');
 
-  app.use(express.logger('dev'));
+var app = express();
+
+app.use(logger('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
   app.listen(process.env.PORT || 5000);
