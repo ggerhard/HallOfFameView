39b552c7e8a1e079f97e54a6ae7b5357d768db37
Gerhard <gerhard@ggoerlich.at>
2017-01-12 01:18:40 +0100

    rebuild

---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hallOfFameViewApp.controller:ProfileCtrl
+ * @description
+ * # ProfileCtrl
+ * Controller of the hallOfFameViewApp
+ */
+angular.module('hallOfFameViewApp')
+  .controller('ProfileCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
